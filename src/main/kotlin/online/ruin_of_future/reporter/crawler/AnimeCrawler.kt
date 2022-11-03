ca7188c2d9966f0acd58503880bac1e9ad0deba3
LinHeLurking <LinHe.Lurking@gmail.com>
2022-11-03 10:31:08 +0800

    refactor: split conflict image wirte function;

--- src/main/kotlin/online/ruin_of_future/reporter/crawler/AnimeCrawler.kt
+++ src/main/kotlin/online/ruin_of_future/reporter/crawler/AnimeCrawler.kt
@@ -81,7 +81,6 @@ object AnimeCrawler {
     private const val entryURL = "https://bangumi.bilibili.com/web_api/timeline_global"
 
     private val byteArrayCacheToday = Cached(byteArrayOf(), 1000 * 60 * 60 * 4L)
-//    private val byteArrayCacheTomorrow = Cached(byteArrayOf(), 1000 * 60 * 60 * 4L)
 
     private val font = Font
         .createFont(Font.TRUETYPE_FONT, this.javaClass.getResourceAsStream("/chinese_font.ttf"))
@@ -100,6 +99,84 @@ object AnimeCrawler {
         return byteArrayCacheToday.isNotOutdated()
     }
 
+    private suspend fun writeOneAnimeInfo(
+        bufferedImage: BufferedImage,
+        animeInfo: AnimeInfo,
+        index: Int,
+        oneAnimeHeight: Int,
+        hBorderWidth: Int,
+        previewHeight: Int,
+        totalImageWidth: Int,
+        titleFont: Font,
+    ) {
+        val curHeight = index * oneAnimeHeight
+
+        val imgY = curHeight + hBorderWidth
+        val oneAnimeImg = withContext(ioDispatcher) {
+            ImageIO.read(URL(animeInfo.coverURL))
+        }
+        val previewWidth = oneAnimeImg.width * previewHeight / oneAnimeImg.height
+        val oneAnimeImgScaled =
+            oneAnimeImg.getScaledInstance(previewWidth, previewHeight, Image.SCALE_SMOOTH)
+        val imgX = if (index % 2 == 0) {
+            20
+        } else {
+            totalImageWidth - 20 - previewWidth
+        }
+
+        val vBorderWidth = if (index % 2 == 0) {
+            imgX
+        } else {
+            totalImageWidth - imgX - previewWidth
+        }
+
+        // borders
+        var g = bufferedImage.createGraphics()
+        g.color = Color.decode("#BDD2B6")
+        g.fillRect(0, curHeight, totalImageWidth, hBorderWidth)
+        g.fillRect(0, curHeight, vBorderWidth, oneAnimeHeight)
+        g.fillRect(0, imgY + previewHeight, totalImageWidth, hBorderWidth)
+        g.fillRect(totalImageWidth - vBorderWidth, curHeight, vBorderWidth, oneAnimeHeight)
+        g.dispose()
+
+
+        // preview image
+        g = bufferedImage.createGraphics()
+        g.drawImage(oneAnimeImgScaled, imgX, imgY, null)
+        g.dispose()
+
+        var titleSize = 65f
+        val titleTextWidth = 750f
+        while (titleSize * animeInfo.title.length > titleTextWidth) {
+            titleSize -= 1
+        }
+
+        // title text
+        val titleY = imgY - 30 + (previewHeight / 2).toInt()
+        val titleX = if (index % 2 == 0) {
+            870 - (animeInfo.title.length / 2 * titleSize).toInt()
+        } else {
+            420 - (animeInfo.title.length / 2 * titleSize).toInt()
+        }
+
+        g = bufferedImage.createGraphics()
+        g.font = titleFont.deriveFont(titleSize)
+        g.color = Color.decode("#4e574c")
+        g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON)
+        g.drawString(animeInfo.title.trim(), titleX, titleY)
+        g.dispose()
+
+        // detailed info
+        val timeY = titleY + 60
+        val timeX = titleX + 5 + (animeInfo.title.length / 2 * titleSize).toInt()
+        g = bufferedImage.createGraphics()
+        g.font = font.deriveFont(35f)
+        g.color = Color.decode("#798777")
+        g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON)
+        g.drawString(animeInfo.pubTime.trim(), timeX, timeY)
+        g.dispose()
+    }
+
     private suspend fun buildImageByteArray(animeInfos: List<AnimeInfo>): ByteArray {
         val hBorderWidth = 18
         val previewHeight = 600
@@ -108,7 +185,7 @@ object AnimeCrawler {
         val totalImageWidth = 1200
 
         val bufferedImage = BufferedImage(totalImageWidth, totalImgHeight, BufferedImage.TYPE_INT_RGB)
-        var g = bufferedImage.createGraphics()
+        val g = bufferedImage.createGraphics()
         g.color = Color.decode("#F8EDE3")
         g.fillRect(0, 0, bufferedImage.width, bufferedImage.height)
         g.dispose()
@@ -119,72 +196,16 @@ object AnimeCrawler {
             if (animeInfo.delay != 0) {
                 return@forEachIndexed
             }
-
-            val curHeight = index * oneAnimeHeight
-
-            val imgY = curHeight + hBorderWidth
-            val oneAnimeImg = ImageIO.read(URL(animeInfo.coverURL))
-            val previewWidth = oneAnimeImg.width * previewHeight / oneAnimeImg.height
-            val oneAnimeImgScaled =
-                oneAnimeImg.getScaledInstance(previewWidth, previewHeight, Image.SCALE_SMOOTH)
-            val imgX = if (index % 2 == 0) {
-                20
-            } else {
-                totalImageWidth - 20 - previewWidth
-            }
-
-            val vBorderWidth = if (index % 2 == 0) {
-                imgX
-            } else {
-                totalImageWidth - imgX - previewWidth
-            }
-
-            // borders
-            g = bufferedImage.createGraphics()
-            g.color = Color.decode("#BDD2B6")
-            g.fillRect(0, curHeight, totalImageWidth, hBorderWidth)
-            g.fillRect(0, curHeight, vBorderWidth, oneAnimeHeight)
-            g.fillRect(0, imgY + previewHeight, totalImageWidth, hBorderWidth)
-            g.fillRect(totalImageWidth - vBorderWidth, curHeight, vBorderWidth, oneAnimeHeight)
-            g.dispose()
-
-
-            // preview image
-            g = bufferedImage.createGraphics()
-            g.drawImage(oneAnimeImgScaled, imgX, imgY, null)
-            g.dispose()
-
-            var titleSize = 65f
-            val titleTextWidth = 750f
-            while (titleSize * animeInfo.title.length > titleTextWidth) {
-                titleSize -= 1
-            }
-
-            // title text
-            val titleY = imgY - 30 + (previewHeight / 2).toInt()
-            val titleX = if (index % 2 == 0) {
-                870 - (animeInfo.title.length / 2 * titleSize).toInt()
-            } else {
-                420 - (animeInfo.title.length / 2 * titleSize).toInt()
-            }
-
-            g = bufferedImage.createGraphics()
-            g.font = titleFont.deriveFont(titleSize)
-            g.color = Color.decode("#4e574c")
-            g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON)
-            g.drawString(animeInfo.title.trim(), titleX, titleY)
-            g.dispose()
-
-            // detailed info
-            val timeY = titleY + 60
-            val timeX = titleX + 5 + (animeInfo.title.length / 2 * titleSize).toInt()
-            g = bufferedImage.createGraphics()
-            g.font = font.deriveFont(35f)
-            g.color = Color.decode("#798777")
-            g.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON)
-            g.drawString(animeInfo.pubTime.trim(), timeX, timeY)
-            g.dispose()
-
+            writeOneAnimeInfo(
+                bufferedImage,
+                animeInfo,
+                index,
+                oneAnimeHeight,
+                hBorderWidth,
+                previewHeight,
+                totalImageWidth,
+                titleFont,
+            )
             delay(100)
         }
 
@@ -220,13 +241,4 @@ object AnimeCrawler {
         byteArrayCacheToday.value = animeByDate(LocalDateTime.now())
         return byteArrayCacheToday.value
     }
-
-//    @Throws(Exception::class)
-//    suspend fun animeTomorrow(): ByteArray {
-//        if (byteArrayCacheTomorrow.isNotOutdated()) {
-//            return byteArrayCacheToday.value
-//        }
-//        byteArrayCacheToday.value = animeByDate(LocalDateTime.now().plusDays(1))
-//        return byteArrayCacheToday.value
-//    }
 }
