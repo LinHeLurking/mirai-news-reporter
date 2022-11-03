bdaab596a3c3952e3a87c815c8bee5f5b797726f
LinHeLurking <LinHe.Lurking@gmail.com>
2022-11-03 10:50:30 +0800

    Add async future in http get operation;

--- src/main/kotlin/online/ruin_of_future/reporter/crawler/HTTPGetter.kt
+++ src/main/kotlin/online/ruin_of_future/reporter/crawler/HTTPGetter.kt
@@ -1,5 +1,7 @@
 package online.ruin_of_future.reporter.crawler
 
+import kotlinx.coroutines.async
+import kotlinx.coroutines.runBlocking
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.io.IOException
@@ -9,10 +11,13 @@ class HTTPGetter {
     private var client = OkHttpClient()
 
     @Throws(IOException::class)
-    suspend fun get(url: String): String {
+    suspend fun get(url: String): String = runBlocking {
         val request: Request = Request.Builder()
             .url(url)
             .build()
-        client.newCall(request).execute().use { response -> return response.body!!.string() }
+        val future = async {
+            client.newCall(request).execute().use { response -> return@async response.body!!.string() }
+        }
+        return@runBlocking future.await()
     }
 }
