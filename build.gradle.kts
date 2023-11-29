6297161af9d10bca9c5c20936fecce1cb20765f1
LinHeLurking <LinHe.Lurking@gmail.com>
2023-11-29 15:57:49 +0800

    fix: config storage overlap;
    Signed-off-by: LinHeLurking <LinHe.Lurking@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "1.8.0"
     kotlin("plugin.serialization") version "1.8.0"
 
-    id("net.mamoe.mirai-console") version "2.15.0"
+    id("net.mamoe.mirai-console") version "2.16.0"
     `maven-publish`
     signing
 }
@@ -21,11 +21,21 @@ dependencies {
     implementation("org.jsoup:jsoup:1.15.3")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.3")
+
+    api(platform("net.mamoe:mirai-bom:2.16.0"))
+    api("net.mamoe:mirai-core-api")     // 编译代码使用
+    runtimeOnly("net.mamoe:mirai-core") // 运行时使用
 }
 
 tasks.compileJava {
     sourceCompatibility = JavaVersion.VERSION_17.toString()
     targetCompatibility = JavaVersion.VERSION_17.toString()
+}
+
+tasks.compileKotlin {
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 //tasks.withType<KotlinCompile>().configureEach {
--- src/main/kotlin/online/ruin_of_future/reporter/data/TouchFishGroupWhiteList.kt
+++ src/main/kotlin/online/ruin_of_future/reporter/data/TouchFishGroupWhiteList.kt
@@ -3,7 +3,7 @@ package online.ruin_of_future.reporter.data
 import net.mamoe.mirai.console.data.AutoSavePluginConfig
 import net.mamoe.mirai.console.data.value
 
-object TouchFishGroupWhiteList : AutoSavePluginConfig("newsGroupWhiteList"), GroupWhiteList {
+object TouchFishGroupWhiteList : AutoSavePluginConfig("touchFishGroupWhiteList"), GroupWhiteList {
     override val groupIdsPerBot: MutableMap<Long, MutableList<Long>> by value()
     override val tag: String
         get() = "TouchFish"
